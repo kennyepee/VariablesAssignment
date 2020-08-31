5946114ff1e24c2ac0f81c50bbfdf322d6d5d28f
Kenny Epee <kennyepee@Kennys-MacBook-Pro.local>
2020-08-31 16:32:05 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int testscore = 50;
+        float price = (float) 24.99;
+        String name = "Bobby Johnson";
+
+        System.out.println("Test score initialized is: " + testscore);
+        System.out.println("Price tag: " + price);
+        System.out.println("Username: " + name);
+    }
+}
